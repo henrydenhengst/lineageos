21dc331a76b8a72b5fa598ed22555c078b4e3356
Henry den Hengst <henrydenhengst@gmail.com>
2023-01-18 14:57:39 +0100

    Create install-fdroid-apps.sh

---
+++ install-fdroid-apps.sh
@@ -0,0 +1,50 @@
+# Install F-droid
+fdroidcl install org.fdroid.fdroid:1015054
+# Install Aurora Store
+fdroidcl install com.aurora.store:41 
+# Install NewPipe
+fdroidcl install org.schabi.newpipe:991
+# Install LibreTube
+fdroidcl install com.github.libretube:26
+# Install Element
+fdroidcl install de.spiritcroc.riotx:40100990
+# Install Briar
+fdroidcl install org.briarproject.briar.android:10419
+# Install Jitsi Meet
+fdroidcl install org.jitsi.meet:227000
+# Install K9 Mail
+fdroidcl install com.fsck.k9:34000
+# Install Syncthing
+fdroidcl install com.nutomic.syncthingandroid:4334
+# Install Organicmaps
+fdroidcl install app.organicmaps:22121812
+# Install Tether-fi
+fdroidcl install be.mygod.vpnhotspot.foss:262
+# Install Antennapod
+fdroidcl install de.danoeh.antennapod:2070195
+# Install Catima
+fdroidcl install me.hackerchick.catima:116
+# Install Odysee
+fdroidcl install com.odysee.floss:69
+# Install KeepassDX
+fdroidcl install com.kunzisoft.keepass.libre:114
+# Install Wookey (Monero wallet)
+fdroidcl install io.wookey.wallet.monero:221
+# Install Firefox Focus
+fdroidcl install org.mozilla.focus:352631859
+# Install Amarok
+fdroidcl install deltazero.amarok.foss:27
+# Install Signal
+fdroidcl install org.thoughtcrime.securesms:112305
+# Simple Book Reader
+fdroidcl install com.github.axet.bookreader:416
+# Collabora Office
+fdroidcl install com.collabora.libreoffice:2147483647
+# LibreAV
+fdroidcl install tech.projectmatris.antimalwareapp:10400
+# Install Aegis Authenticator 2FA
+fdroidcl install com.beemdevelopment.aegis:56
+# Install eSpeak TTS engine voor Organicsmaps
+fdroidcl install com.reecedunn.espeak:21
+# Install rhvoice TTS engine
+fdroidcl install com.github.olga_yakovleva.rhvoice.android:108000
